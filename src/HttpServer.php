13911f2d0a22eb31afcc8c8ee4ace0e7d5bf7bc5
子旭 苏 <fdream.lhl@foxmail.com>
2019-10-30 11:16:05 +0800

    generate request_id

--- src/HttpServer.php
+++ src/HttpServer.php
@@ -261,11 +261,19 @@ class HttpServer {
         ++ self::$worker->worker_http_request_counter;
         $count = self::$http_request_counter->add();
 
-        $request_time = $request->server['request_time_float'];
-        $micro_second = substr(sprintf('%.3f', $request_time - floor($request_time)), 2);
-        $request_string = date("[H:i:s.$micro_second]", $request_time) . self::$worker_id . "-{$count} " .
-             ($request->header['x-forwarded-for'] ?? $request->server['remote_addr']) . ' ' .
-             $request->server['request_method'] . ' ' . ($request->header['host'] ?? '') .
+        $request_time_float = $request->server['request_time_float'];
+        $request_time = (int)$request_time_float;
+        $client_ip = $request->header['x-forwarded-for'] ?? $request->server['remote_addr'];
+        $client_ip_int = ip2long($client_ip);
+        $local_ip_right = ip2long(Environment::getServiceConfig()->local_ip) % 0x10000;
+        $request_id = sprintf('%08x-%04x-4%03x-%x%03x-%07x%05x', $client_ip_int, $local_ip_right, mt_rand(0, 0xFFF),
+            mt_rand(8, 0xB), mt_rand(0, 0xFFF), ((int)$request_time) >> 4, $count % 0x100000);
+        \SysContext::set('request_id', $request_id);
+        $response->header('X-Request-ID', $request_id);
+
+        $micro_second = substr(sprintf('%.3f', $request_time_float - $request_time), 2);
+        $request_string = date("[H:i:s.$micro_second]", $request_time) . self::$worker_id . "-{$count} " . $client_ip .
+             ' ' . $request->server['request_method'] . ' ' . ($request->header['host'] ?? '') .
              $request->server['request_uri'] . (isset($request->server['query_string']) ? '?' .
              $request->server['query_string'] : '');
 
